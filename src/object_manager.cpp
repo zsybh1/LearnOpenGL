80a99117b0de72ff46771e18388ebbcbe37bb27d
zsybh1 <zsybh1@163.com>
2020-07-09 16:35:16 +0800

    Up

--- src/object_manager.cpp
+++ src/object_manager.cpp
@@ -151,10 +151,10 @@ void Object_Manager::load(Object e) {
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
-      //  glEnableVertexAttribArray(1);
-      //  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
-      //  glEnableVertexAttribArray(2);
-      //  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
         break;
     }
     default:
